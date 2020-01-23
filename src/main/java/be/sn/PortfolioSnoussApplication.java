506aae17bf85aedd354760dec5934024ffcc88f8
SnYoussef <snyoussef07@gmail.com>
2020-01-23 15:57:14 +0100

    Start Project

---
+++ src/main/java/be/sn/PortfolioSnoussApplication.java
@@ -0,0 +1,13 @@
+package be.sn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortfolioSnoussApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PortfolioSnoussApplication.class, args);
+    }
+
+}
---
+++ src/test/java/be/sn/PortfolioSnoussApplicationTests.java
@@ -0,0 +1,13 @@
+package be.sn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfolioSnoussApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
