c98299ef08925f1acd975b639adbf848ec5a3ca7
SnYoussef <snyoussef07@gmail.com>
2020-01-23 17:06:01 +0100

    deactivate security for the moment

--- src/main/java/be/sn/PortfolioSnoussApplication.java
+++ src/main/java/be/sn/PortfolioSnoussApplication.java
@@ -8,11 +8,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
 import java.util.ArrayList;
 import java.util.List;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class PortfolioSnoussApplication implements CommandLineRunner {
 
     @Autowired
